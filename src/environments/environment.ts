cbb43b6d15ea6f72c820d4a26123310c26cdfa48
SypeCode <sypecr@gmail.com>
2020-10-04 23:06:18 -0600

    Log in terminado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBwx19XpOo8yv0WVG4PTbrM5yiigXgsF5c",
+    authDomain: "workshop-alex.firebaseapp.com",
+    databaseURL: "https://workshop-alex.firebaseio.com",
+    projectId: "workshop-alex",
+    storageBucket: "workshop-alex.appspot.com",
+    messagingSenderId: "717371806868",
+    appId: "1:717371806868:web:2ea392677b57b9f86d1081",
+    measurementId: "G-TWSH57Y2QZ"
+  }
 };
 
 /*
